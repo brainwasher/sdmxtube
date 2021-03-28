7e69238b3def2795d7aee4d4aa3a097c769346a0
Daniel Suranyi <d@daniels.tv>
2021-03-28 09:16:35 +0200

    cosmetics

--- sdmx2gv/runConversion.ps1
+++ sdmx2gv/runConversion.ps1
@@ -1,33 +1,33 @@
-echo "..."
+Write-Output "..."
 
 <# TODO: get structure file as CL parameter #>
 <# TODO: get structure from REST web service as alternative (CL parameter with URL)#>
 java -jar $PSScriptRoot/../bin-java/SaxonHE/saxon-he-10.3.jar -s:$PSScriptRoot/StructureMap.xml -xsl:$PSScriptRoot/structuremap2erm.xslt -o:$PSScriptRoot/structuremap2erm.gv
 
-echo ""
-echo "GV done."
-echo ""
+Write-Output ""
+Write-Output "GV done."
+Write-Output ""
 
 IF($IsWindows) {
-    echo "Running on Windows: local portable graphviz used..."
+    Write-Output "Running on Windows: local portable graphviz used..."
     & $PSScriptRoot/../bin-win/graphviz/dot.exe -Tsvg $PSScriptRoot/structuremap2erm.gv -O
     & $PSScriptRoot/../bin-win/graphviz/dot.exe -Tpng $PSScriptRoot/structuremap2erm.gv -O
 } ELSEIF($IsLinux) {
-    echo "Running on Linux: trying installed graphviz package..."
+    Write-Output "Running on Linux: trying installed graphviz package..."
     try {
         & dot -Tsvg $PSScriptRoot/structuremap2erm.gv -O
         & dot -Tpng $PSScriptRoot/structuremap2erm.gv -O            
     }
     catch {
-        echo "Conversion failed, is graphviz installed? Install graphviz depending on your distro: "
-        echo "   Debian: sudo apt-get install graphviz"
-        echo ""
+        Write-Output "Conversion failed, is graphviz installed? Install graphviz depending on your distro: "
+        Write-Output "   Debian: sudo apt-get install graphviz"
+        Write-Output ""
         throw "script terminated. astalavista baby."
     }
 }
 
-echo ""
-echo "Conversion done."
+Write-Output ""
+Write-Output "Conversion done."
 
-echo ""
-echo ""
+Write-Output ""
+Write-Output ""
--- xslt-test/runSimple.ps1
+++ xslt-test/runSimple.ps1
@@ -1,11 +1,11 @@
-echo "..."
+Write-Output "..."
 
 java -jar $PSScriptRoot/../bin-java/SaxonHE/saxon-he-10.3.jar -s:$PSScriptRoot/simple.xml -xsl:$PSScriptRoot/simple.xslt -o:$PSScriptRoot/simple.txt
 
-echo ""
-echo "Done."
-echo ""
-echo "Output:"
-echo ""
+Write-Output ""
+Write-Output "Done."
+Write-Output ""
+Write-Output "Output:"
+Write-Output ""
 Get-Content simple.txt
-echo ""
+Write-Output ""
